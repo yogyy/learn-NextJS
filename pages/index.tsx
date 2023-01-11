b951283b8aee6fc0e6fa1a153f866b25d945d7ab
yogyy <akunwanhut.0y@gmail.com>
2023-01-11 13:30:00 +0700

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleOrder = () => {
+    router.replace("/product");
+  };
   return (
     <Layout pageTitle={"Home Page"}>
       <div className={styles.content}>
-        <h1>Hi, i'm Yogi</h1>
+        <h1>Hi, i'm Muhammad Yogi Firman Syah</h1>
         <p>
           I'm a <span className={styles.textgrad}>Frontend</span> Developer
         </p>
@@ -13,13 +20,18 @@ export default function Home() {
           Saya adalah seorang website developer yang memiliki kemampuan dalam
           membuat dan mengembangkan situs web yang fungsional dan berkualitas.
           Saya memiliki pengalaman dalam menggunakan berbagai bahasa pemrograman
-          seperti HTML, CSS, JavaScript, dan serta framework React seperti
-          NextJS. Saya juga memiliki kemampuan dalam menggunakan alat seperti
-          Git, GitHub, dan command line untuk mengelola proyek dan mengelola
-          versi kode sumber. Saya senang belajar dan selalu terbuka untuk
-          mencoba teknologi baru untuk memastikan bahwa situs web yang saya
-          kembangkan selalu menggunakan teknologi terbaru dan terbaik
+          seperti <span className={styles.textgrad}>HTML, CSS, JavaScript</span>
+          , dan serta framework React seperti NextJS. Saya juga memiliki
+          kemampuan dalam menggunakan alat seperti Git, GitHub, dan command line
+          untuk mengelola proyek dan mengelola versi kode sumber. Saya senang
+          belajar dan selalu terbuka untuk mencoba teknologi baru untuk
+          memastikan bahwa situs web yang saya kembangkan selalu menggunakan
+          teknologi terbaru dan terbaik
         </p>
+        <div>
+          <button onClick={handleOrder}>place order</button>
+          <Link href={"/posts"}>post page</Link>
+        </div>
       </div>
       {/* <Image src={"/iamge1.webp"} alt={"gambar1"} width={700} height={400} /> */}
     </Layout>
