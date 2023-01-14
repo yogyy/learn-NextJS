0e5cbab00f8a826464be0ad6d4d0d97f53292a33
yogyy <yogimr261@gmail.com>
2023-01-14 22:14:24 +0700

    edit footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -28,20 +28,24 @@ export default function Footer() {
     }
   }
   return (
-    <div>
-      <footer className={styles.container}>
-        <p>@constantine</p>
-        <ul>
-          <li>
-            <button type="button" onClick={otHome}>
+    <div className={styles.container}>
+      <footer className={styles.footer}>
+        <p className={styles.p}>@yogyy</p>
+        <ul className={styles.ul}>
+          <li className={styles.list}>
+            <button className={styles.button89} type="button" onClick={otHome}>
               home
             </button>
           </li>
-          <li>
-            <button onClick={otUser}>users</button>
+          <li className={styles.list}>
+            <button className={styles.button89} onClick={otUser}>
+              users
+            </button>
           </li>
-          <li>
-            <button onClick={otBlogs}>blogs</button>
+          <li className={styles.list}>
+            <button className={styles.button89} onClick={otBlogs}>
+              blogs
+            </button>
           </li>
         </ul>
       </footer>
