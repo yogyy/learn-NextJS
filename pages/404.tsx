4d1a7eb69f4a5cf50113d2732a6914bf967aa860
yogyy <yogimr261@gmail.com>
2023-01-24 14:44:41 +0700

    404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,39 +1,33 @@
-import Head from "next/head";
-import Link from "next/link";
-import useRouter from "next/router";
-import React, { useEffect } from "react";
+import Head from 'next/head';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
+import React, {useEffect} from 'react';
+import Layout from '../components/Layout';
+import styles from '../styles/404.module.css';
 
 export default function Custom404() {
-  const router = useRouter;
+  const router = useRouter();
   useEffect(() => {
     setTimeout(() => {
-      router.push("/");
-      console.log("dipannggil setelah 2.5 detik");
-    }, 2500);
+      router.push('/');
+      console.log('not found 404');
+    }, 5500);
   }, []);
-
-  function backHome() {
-    return (event: React.MouseEvent) => {
-      router.push("/");
-    };
-  }
 
   return (
     <>
       <Head>
-        <title>not found bro</title>
+        <title>404 Not Found</title>
         <meta name="description" content="website NextJS Basic" />
       </Head>
-      <div className="title_404">
-        <h1>whooooooooopss.</h1>
-        <h2>Page not found 505</h2>
-        <p>
-          {/* <Link className="return_home" href={"/"}>
-      Home
-    </Link> */}
-          <i>auto redirect to home in 2second</i>
-        </p>
-        {/* <button onClick={backHome()}>Pulang ke home</button> */}
+      <div className={styles.page404}>
+        <p className={styles.protate}>i'm going back to</p>
+        <Link href="/">
+          <h1 className={styles.h1}>404</h1>
+        </Link>
+
+        <p>This page could not be found.</p>
+        <p></p>
       </div>
     </>
   );
