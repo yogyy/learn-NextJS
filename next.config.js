a03b0cba8499b4dbf0d7af88be235c913c51150d
yogyy <yogimr261@gmail.com>
2023-01-25 14:57:30 +0700

    ..

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-}
+  reactStrictMode: false, //default true
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
