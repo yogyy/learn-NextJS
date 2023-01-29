845b928000372abd3510d01bb969586680c1bc06
yogyy <yogimr261@gmail.com>
2023-01-29 07:22:12 +0700

    separate image count

---
+++ components/countAge.ts
@@ -0,0 +1,10 @@
+export function ageCounter(birthdate: number | string) {
+  var today = new Date();
+  var birthDate = new Date(birthdate);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
