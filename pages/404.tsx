69fd3e1b84fc41f7f93086099a337e0bcb4ba80e
yogyy <yogimr261@gmail.com>
2023-02-16 01:45:29 +0700

    edit 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import {useRouter} from 'next/router';
 import React, {useEffect} from 'react';
-import styles from '../styles/404.module.css';
+import {Accent1} from '../components/links/Accent';
 
 export default function Custom404() {
   const router = useRouter();
@@ -20,13 +20,13 @@ export default function Custom404() {
         <title>404 Not Found</title>
         <meta name="description" content="website NextJS Basic" />
       </Head>
-      <div className={styles.page404}>
-        <p className={styles.protate}>i&apos;m going back to</p>
+      <div className="h-screen flex flex-col items-center justify-center">
         <Link href="/">
-          <h1 className={styles.h1}>404</h1>
+          <h1 className="text-9xl">
+            <Accent1>404</Accent1>
+          </h1>
         </Link>
-        <p>This page could not be found.</p>
-        <p></p>
+        <p className="text-xl text text-red-500">This page could not be found.</p>
       </div>
     </>
   );
