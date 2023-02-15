74d28db67f33bc5e0812685083384e3ba3d9ec39
yogyy <yogimr261@gmail.com>
2023-02-15 19:43:41 +0700

    logo

---
+++ components/links/LogoName.tsx
@@ -0,0 +1,9 @@
+import {Accent1} from './Accent';
+
+export const LogoLink = () => {
+  return (
+    <Accent1>
+      <span className="text-2xl md:text-4xl 2xl:text-3xl">ϒ</span>ogyy
+    </Accent1>
+  );
+};
