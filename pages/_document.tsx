29e472a33568f859969ec236a26f9c219732d1a2
yogyy <yogimr261@gmail.com>
2023-02-16 21:56:15 +0700

    config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import {Html, Head, Main, NextScript} from 'next/document';
 
 export default function Document() {
   return (
-    <Html className="dark" lang="en">
+    <Html lang="en">
       <Head />
       <body className="bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-200">
         <Main />
