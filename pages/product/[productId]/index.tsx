a3e5e3743ea66b97d302dc99841d9b19c2f2c11e
yogyy <yogimr261@gmail.com>
2023-02-19 21:23:11 +0700

    learn ISR

--- pages/product/[productId]/index.tsx
+++ pages/product/[productId]/index.tsx
@@ -1,15 +1,76 @@
+import clsx from 'clsx';
 import {useRouter} from 'next/router';
 import Layout from '../../../components/Layout';
+import Loading from '../../../components/loading';
 
-export default function ProductDetail() {
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+}
+interface ProductDetailProps {
+  product: Product;
+}
+
+export default function ProductDetail({product}: ProductDetailProps) {
   const router = useRouter();
-  const productId = router.query.productId;
+
+  if (router.isFallback) {
+    return <Loading />;
+  }
   return (
-    <Layout pageTitle={`product - ${productId}`}>
-      <div className="produkid" style={{paddingTop: '80px'}}>
-        <h1>details about product {productId}</h1>
-        <button onClick={() => router.back()}>back</button>
+    <Layout pageTitle={`product - ${product.id}`}>
+      <div className="layout pt-32">
+        <div className="produkid flex flex-col">
+          <h1 className="text-2xl">details about product {product.id}</h1>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p className="underline font-semibold">price : {product.price}</p>
+          <button
+            className={clsx(
+              'mt-5 w-fit',
+              'inline-flex items-center gap-1 text-sm font-medium md:text-base',
+              ' text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white',
+              'focus:outline-none focus-visible:ring focus-visible:ring-primary-300',
+              'transition-colors'
+            )}
+            onClick={() => router.back()}>
+            back
+          </button>
+        </div>
       </div>
     </Layout>
   );
+}
+
+interface GetStaticProps {
+  params: {
+    productId: string;
+  };
+}
+
+export async function getStaticProps(context: GetStaticProps) {
+  const {productId} = context.params;
+  const res = await fetch(`http://localhost:9999/products/${productId}`);
+  const data = await res.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      product: data,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{params: {productId: '1'}}],
+    fallback: true,
+  };
 }
--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,18 +1,66 @@
+// import clsx from 'clsx';
 import Link from 'next/link';
 import Layout from '../../components/Layout';
+// import UnstyledLink from '../../components/links/UnstyledLink';
 
-export default function ProductList() {
+export default function ProductList({products}: any) {
+  // const items = [];
+  // for (let i = 0; i < 50; i++) {
+  //   items.push(`product ${i + 1}`);
+  // }
+  // const limitedItems = items.slice(0, 30);
+  // return (
+  //   <div className="product-box">
+  //     <Layout pageTitle={`product`}>
+  //       <div className="pt-24 layout">
+  //         <div className="pt-5 grid grid-cols-3 gap-4">
+  //           {limitedItems.map((item, index) => (
+  //             <UnstyledLink
+  //               key={index}
+  //               href={`/product/${index + 1}`}
+  //               className={clsx(
+  //                 'inline-flex items-center gap-1 text-sm font-medium md:text-base',
+  //                 'text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white',
+  //                 'focus:outline-none focus-visible:ring focus-visible:ring-primary-300',
+  //                 'transition-colors'
+  //               )}>
+  //               {item}
+  //             </UnstyledLink>
+  //           ))}
+  //         </div>
+  //       </div>
+  //     </Layout>
+  //   </div>
+  // );
   return (
-    <div className="product-box">
-      <Layout pageTitle={`product`}>
-        <div style={{padding: '80px'}} className="produklist">
-          <Link href="/product/1">product 1</Link>
-          <Link href="/product/2">product 2</Link>
-          <Link href="/product/3" replace>
-            product 3
-          </Link>
+    <>
+      <Layout pageTitle="Product">
+        <div className="layout pt-28 min-h-full">
+          <h1>List Products</h1>
+          {products.map((product: any) => {
+            return (
+              <div className="mt-5" key={product.id}>
+                <Link href={`product/${product.id}`}>
+                  {product.id} {product.title} price:{product.price}
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </Layout>
-    </div>
+    </>
   );
+}
+
+export async function getStaticProps() {
+  console.log('generating / regenerating productlist');
+  const res = await fetch('http://localhost:9999/products');
+  const data = await res.json();
+
+  return {
+    props: {
+      products: data,
+    },
+    revalidate: 30,
+  };
 }
