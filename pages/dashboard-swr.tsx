443b4c79d70b9c1610a9a2edb5081640f46f84cb
yogyy <yogimr261@gmail.com>
2023-02-20 20:50:20 +0700

    Client side Data Fetching with SWR

---
+++ pages/dashboard-swr.tsx
@@ -0,0 +1,31 @@
+import useSWR from 'swr';
+
+const fetcher = async () => {
+  const res = await fetch('http://localhost:9999/dashboard');
+  const data = await res.json();
+  return data;
+};
+
+interface DashboardData {
+  posts: number;
+  likes: number;
+  follower: number;
+  following: number;
+}
+
+export default function DashboardSwr() {
+  const {data, error} = useSWR<DashboardData>('dashboard', fetcher);
+
+  if (error) return 'A error occured ';
+  if (!data) return 'Sabar...';
+
+  return (
+    <div className="layout">
+      <h2>Dashboard</h2>
+      <p>Posts - {data.posts}</p>
+      <p>Likes - {data.likes}</p>
+      <p>Follower - {data.follower}</p>
+      <p>Following - {data.following}</p>
+    </div>
+  );
+}
