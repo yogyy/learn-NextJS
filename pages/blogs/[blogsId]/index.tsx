db3bd18187981714c948b186302034e242e85c75
yogyy <yogimr261@gmail.com>
2023-02-24 18:52:19 +0700

    dummy blogs

---
+++ pages/blogs/[blogsId]/index.tsx
@@ -0,0 +1,89 @@
+import clsx from 'clsx';
+import {useRouter} from 'next/router';
+import Layout from '../../../components/Layout';
+import Loading from '../../../components/loading';
+
+interface Blog {
+  id: number;
+  title: string;
+  body: string;
+}
+interface UserDetailProps {
+  blog: Blog;
+}
+
+export default function UserDetail(props: UserDetailProps) {
+  const router = useRouter();
+  const {blog} = props;
+
+  // if (router.isFallback) {
+  //   return (
+  //     <>
+  //       <Loading />
+  //     </>
+  //   );
+  // }
+
+  return (
+    <Layout pageTitle={`blog post - ${blog.title}`}>
+      <div className="layout py-28">
+        <button
+          type="button"
+          className={clsx(
+            'bg-blue-200 dark:bg-blue-900 font-semibold px-2 py-1 rounded-md',
+            'hover:text-sky-200'
+          )}
+          onClick={() => router.push('/blogs')}>
+          back
+        </button>
+        {router.isFallback && <Loading />}
+        {!router.isFallback && (
+          <div className="h-full w-1/3 p-5 mt-5 rounded-md bg-blue-200 dark:bg-blue-900">
+            <div className="flex flex-col gap-3 font-semibold">
+              <p>{blog.title}</p>
+              <p>{blog.body}</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+  const dataUsers = await res.json();
+
+  const paths = dataUsers.map((blog: Blog) => ({
+    params: {
+      blogsId: `${blog.id}`,
+    },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+interface GetStaticProps {
+  params: {
+    blogsId: string;
+  };
+}
+export async function getStaticProps(context: GetStaticProps) {
+  const {blogsId} = context.params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${blogsId}`);
+  const blog = await res.json();
+
+  if (!blog.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      blog,
+    },
+  };
+}
--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import clsx from 'clsx';
+import {useRouter} from 'next/router';
+import React, {useEffect, useState} from 'react';
 import Layout from '../../components/Layout';
-import styles from '../../styles/Blog.module.css';
+import Accent from '../../components/links/Accent';
+import Loading from '../../components/loading';
 
 type NewType = {
   id: number;
@@ -8,34 +11,81 @@ type NewType = {
   body: string;
 };
 
-type Post = NewType;
-type BlogProps = {
-  dataBlog: Post[];
-};
+// type Post = NewType;
+// type BlogProps = {
+//   dataBlog: Post[];
+// };
 
-export default function Blog(props: BlogProps) {
-  const {dataBlog} = props;
+// export default function Blog(props: BlogProps) {
+//   const {dataBlog} = props;
+//   return (
+//     <Layout pageTitle={'Blog Page'}>
+//       <div className={styles.div}>
+//         <h1>List Blog</h1>
+//         {dataBlog.map((blog) => (
+//           <div key={blog.id} className={styles.card}>
+//             <h3>{blog.title}</h3>
+//             <p>{blog.body}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </Layout>
+//   );
+// }
+
+// export async function getServerSideProps() {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+//   const dataBlog = await res.json();
+//   return {
+//     props: {
+//       dataBlog,
+//     },
+//   };
+// }
+
+export default function Blog() {
+  const router = useRouter();
+  const [dataBlog, setDataBlog] = useState<NewType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=21');
+      const data = await res.json();
+      setDataBlog(data);
+      setIsLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <Layout pageTitle={'Blog Page'}>
-      <div className={styles.div}>
-        <h1>List Blog</h1>
-        {dataBlog.map((blog) => (
-          <div key={blog.id} className={styles.card}>
-            <h3>{blog.title}</h3>
-            <p>{blog.body}</p>
+      <div className={`text-gray-500 dark:text-gray-300 layout pt-24`}>
+        <h1>
+          <Accent>List Blog</Accent>
+        </h1>
+        {isLoading ? (
+          <div>
+            <Loading />
           </div>
-        ))}
+        ) : (
+          <div className="mt-4 grid gap-4 sm:grid-cols-2 xl:grid-cols-3 ">
+            {dataBlog.map((blog) => (
+              <div key={blog.id} className="mt-4">
+                <div
+                  onClick={() => router.push(`/blogs/${blog.id}`)}
+                  className={clsx(
+                    'bg-gray-100 dark:bg-gray-700  p-5 rounded-lg h-full flex flex-col justify-between',
+                    'hover:cursor-pointer hover:shadow-sky-600 shadow-lg'
+                  )}>
+                  <h2 className="text-sky-500 text-xl">{blog.title}</h2>
+                  <p className="mt-5">{blog.body}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </Layout>
   );
-}
-
-export async function getServerSideProps() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
-  const dataBlog = await res.json();
-  return {
-    props: {
-      dataBlog,
-    },
-  };
 }
