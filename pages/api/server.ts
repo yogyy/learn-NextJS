c3c80aaa087a7330cd5157ece3cb7d322c7eb356
yogyy <yogimr261@gmail.com>
2023-03-17 17:01:47 +0700

    fake server

---
+++ pages/api/server.ts
@@ -0,0 +1,20 @@
+import * as jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id',
+  })
+);
+server.use(router);
+
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
+
+export default server;
